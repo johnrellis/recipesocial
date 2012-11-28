233951278447055c381e707183ee9dec7cb95e7a
John Rellis <john.d.rellis@gmail.com>
2012-11-28 10:50:16 +0000

    can add simple recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,17 @@
-class RecipeController < ApplicationController
+class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.build(params[:recipe])
+    if @recipe.save
+      flash[:success] = "Recipe created!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
 end
