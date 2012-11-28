4c9c1cc4869701becea33debc37d73b0c7179cca
John Rellis <john.d.rellis@gmail.com>
2012-11-28 09:13:40 +0000

    added recipes controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ActiveRecord::Base
   attr_accessible :method, :name, :user_id
   belongs_to :user
 
+  validates :user_id, presence: true
   validates :name, presence: true
   validates :method, presence: true
 end
