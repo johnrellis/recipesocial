ab44e0dd2552d972ee5fd89aee0601328d89fb67
John Rellis <john.d.rellis@gmail.com>
2012-12-30 16:20:17 +0000

    changed login homepage to be /home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user
       if user.authenticate(params[:session][:password])
         sign_in user
-        redirect_to user
+        redirect_to root_path
       else
         flash[:error] = "Sorry, your password is incorrect"
         redirect_to signin_path(email: @email)
